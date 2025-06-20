d6787d1d7c21d21a5403abb902d29d87dbc8e104
Code__R <103403881+roudramondal@users.noreply.github.com>
2025-06-20 23:48:38 +0600

    Fibonacci series in using Tree

---
+++ Fibonacci_Recursion.cpp
@@ -0,0 +1,17 @@
+// Febonacci relation fib (n)=fib (n-1)+fib(n-2)
+// Base case fib (0)=0,fib (1)=1
+ #include<bits/stdc++.h>
+ using namespace std;
+ int fibo(int n){
+    // Base case 
+    if (n==0)return 0;
+    if (n==1)return 1;
+    // Function body
+    int result = fibo (n-1)+fibo (n-2);
+    return result;
+
+ }
+ int main (){
+    //cout<<fibo(0)<<" "<<fibo(1)<<" "<<fibo(2)<<" "<<fibo(3)<<" "<<fibo(4)<<" "<<fibo(5)<<" "<<fibo(6)<<" "<<fibo(7)<<endl;
+    return 0;
+}
