16e1b34ab101dac79d845346a5091c07c11516c3
Code__R <103403881+roudramondal@users.noreply.github.com>
2025-06-27 18:17:34 +0600

    Update Fibonacci_Recursion.cpp

--- Fibonacci_Recursion.cpp
+++ Fibonacci_Recursion.cpp
@@ -7,7 +7,7 @@
     if (n==0)return 0;
     if (n==1)return 1;
     // Function body
-    int result = fibo (n-1)+fibo (n-2);
+    int result = fibo (n-1)+fibo (n-2); // this the method of fibonacci series
     return result;
 
  }
